250cc67f5006d70383f4e54f424e0334208e96c5
macos <macos@macoss-MacBook-Pro.local>
2018-11-14 14:58:36 -0200

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by macos on 14/11/18.
+//  Copyright © 2018 macos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
